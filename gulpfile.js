7185cf9d1287fcfd7944a04127fd6ad7da64f1d3
Zhenya <fedyai.eugen@gmail.com>
2018-06-19 12:01:20 +0300

    Add rollup js module bundler to gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -11,57 +11,71 @@ const mqpacker = require(`css-mqpacker`);
 const minify = require(`gulp-csso`);
 const rename = require(`gulp-rename`);
 const imagemin = require(`gulp-imagemin`);
+const rollup = require(`gulp-better-rollup`);
+const sourcemaps = require(`gulp-sourcemaps`);
 
 gulp.task(`style`, () => {
   return gulp.src(`sass/style.scss`).
-    pipe(plumber()).
-    pipe(sass()).
-    pipe(postcss([
-      autoprefixer({
-        browsers: [
-          `last 1 version`,
-          `last 2 Chrome versions`,
-          `last 2 Firefox versions`,
-          `last 2 Opera versions`,
-          `last 2 Edge versions`
-        ]
-      }),
-      mqpacker({sort: true})
-    ])).
-    pipe(gulp.dest(`build/css`)).
-    pipe(server.stream()).
-    pipe(minify()).
-    pipe(rename(`style.min.css`)).
-    pipe(gulp.dest(`build/css`));
+  pipe(plumber()).
+  pipe(sass()).
+  pipe(postcss([
+    autoprefixer({
+      browsers: [
+        `last 1 version`,
+        `last 2 Chrome versions`,
+        `last 2 Firefox versions`,
+        `last 2 Opera versions`,
+        `last 2 Edge versions`
+      ]
+    }),
+    mqpacker({
+      sort: true
+    })
+  ])).
+  pipe(gulp.dest(`build/css`)).
+  pipe(server.stream()).
+  pipe(minify()).
+  pipe(rename(`style.min.css`)).
+  pipe(gulp.dest(`build/css`));
 });
 
 gulp.task(`scripts`, () => {
   return gulp.src(`js/**/*.js`).
-    pipe(plumber()).
-    pipe(gulp.dest(`build/js/`));
+  pipe(plumber()).
+  pipe(sourcemaps.init()).
+  // use IIFE format
+  pipe(rollup({}, `iife`)).
+  pipe(sourcemaps.write(``)).
+  pipe(gulp.dest(`build/js/`));
 });
 
 gulp.task(`imagemin`, [`copy`], () => {
   return gulp.src(`build/img/**/*.{jpg,png,gif}`).
-    pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true})
-    ])).
-    pipe(gulp.dest(`build/img`));
+  pipe(imagemin([
+    imagemin.optipng({
+      optimizationLevel: 3
+    }),
+    imagemin.jpegtran({
+      progressive: true
+    })
+  ])).
+  pipe(gulp.dest(`build/img`));
 });
 
 gulp.task(`copy-html`, () => {
   return gulp.src(`*.{html,ico}`).
-    pipe(gulp.dest(`build`)).
-    pipe(server.stream());
+  pipe(gulp.dest(`build`)).
+  pipe(server.stream());
 });
 
 gulp.task(`copy`, [`copy-html`, `scripts`, `style`], () => {
   return gulp.src([
     `fonts/**/*.{woff,woff2}`,
     `img/*.*`
-  ], {base: `.`}).
-    pipe(gulp.dest(`build`));
+  ], {
+    base: `.`
+  }).
+  pipe(gulp.dest(`build`));
 });
 
 gulp.task(`clean`, () => {
@@ -99,5 +113,4 @@ gulp.task(`build`, [`assemble`], () => {
   gulp.start(`imagemin`);
 });
 
-gulp.task(`test`, () => {
-});
+gulp.task(`test`, () => {});

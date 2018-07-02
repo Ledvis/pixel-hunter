7c08c71a416e3fa890e2791fcc7324f2cac8ca14
Zhenya <fedyai.eugen@gmail.com>
2018-07-02 08:58:00 +0300

    Add data module

--- js/data.js
+++ js/data.js
@@ -1,40 +1,29 @@
-const levelsData = {
-  'level-1': {
+const imagesURL = {
+  paintings: [
+    `https://k42.kn3.net/CF42609C8.jpg`,
+    `https://k42.kn3.net/D2F0370D6.jpg`,
+    `https://k32.kn3.net/5C7060EC5.jpg`,
+  ],
+  photos: [
+    `http://i.imgur.com/1KegWPz.jpg`,
+    `https://i.imgur.com/DiHM5Zb.jpg`,
+    `http://i.imgur.com/DKR1HtB.jpg`,
+  ]
+};
+
+const levelsType = {
+  'single': {
     question: `Угадайте для каждого изображения фото или рисунок?`,
-    type: `double`,
-    images: new Set(
-        `https://k42.kn3.net/CF42609C8.jpg`,
-        `http://i.imgur.com/1KegWPz.jpg`
-    ),
-    answers: new Set(
-        `painting`,
-        `photo`
-    )
-  },
-  'level-2': {
-    question: `Угадай, фото или рисунок?`,
-    type: `single`,
-    images: [{
-      url: `https://k42.kn3.net/D2F0370D6.jpg`,
+    answers: [{
+      image: imagesURL.paintings[0],
       type: `painting`
     }]
   },
-  'level-3': {
+  'double': {
+    question: `Угадай, фото или рисунок?`,
+  },
+  'triple': {
     question: `Найдите рисунок среди изображений`,
-    type: `triple`,
-    images: [{
-      url: `https://k32.kn3.net/5C7060EC5.jpg`,
-      type: `painting`
-    },
-    {
-      url: `https://i.imgur.com/DiHM5Zb.jpg`,
-      type: `photo`
-    },
-    {
-      url: `http://i.imgur.com/DKR1HtB.jpg`,
-      type: `photo`
-    }
-    ]
   }
 };
 

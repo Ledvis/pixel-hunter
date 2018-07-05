0e493e8fdec5e76197239e5e04e229970eb8909c
Zhenya <fedyai.eugen@gmail.com>
2018-07-05 20:19:13 +0300

    Refactor game module

--- js/game.js
+++ js/game.js
@@ -9,89 +9,93 @@ import {
 import footerTemplate from './page-footer';
 import renderStats from './stats';
 
-let html = ``;
-
-function createGameLevel(levels) {
-  for (const level of levels) {
-    switch (level.type) {
-      case `single`:
-        html = getElementFromTemplate(`
-          <div class="game">
-            <p class="game__task">${level.question}</p>
-            <form class="game__content  game__content--wide">
-              <div class="game__option">
-                <img src="${level.answers[0].image}" alt="Option 1" width="705" height="455">
-                <label class="game__answer  game__answer--photo">
-                  <input name="question1" type="radio" value="photo">
-                  <span>Фото</span>
-                </label>
-                <label class="game__answer  game__answer--wide  game__answer--paint">
-                  <input name="question1" type="radio" value="paint">
-                  <span>Рисунок</span>
-                </label>
-              </div>
-            </form>
+function fillGameLevel(level) {
+  let html = ``;
+  switch (level.type) {
+    case `single`:
+      html = `
+        <p class="game__task">${level.question}</p>
+        <form class="game__content  game__content--wide">
+          <div class="game__option">
+            <img src="${level.answers[0].image}" alt="Option 1" width="705" height="455">
+            <label class="game__answer  game__answer--photo">
+              <input name="question1" type="radio" value="photo">
+              <span>Фото</span>
+            </label>
+            <label class="game__answer  game__answer--wide  game__answer--paint">
+              <input name="question1" type="radio" value="paint">
+              <span>Рисунок</span>
+            </label>
           </div>
-          ${footerTemplate}
-        `);
-        break;
-      case `double`:
-        html = getElementFromTemplate(`
-            <div class="game">
-              <p class="game__task">${level.question}</p>
-              <form class="game__content">
-                <div class="game__option">
-                  <img src="${level.answers[0].image}" alt="Option 1" width="468" height="458">
-                  <label class="game__answer game__answer--photo">
-                    <input name="question1" type="radio" value="photo">
-                    <span>Фото</span>
-                  </label>
-                  <label class="game__answer game__answer--paint">
-                    <input name="question1" type="radio" value="paint">
-                    <span>Рисунок</span>
-                  </label>
-                </div>
-                <div class="game__option">
-                  <img src="${level.answers[1].image}" alt="Option 2" width="468" height="458">
-                  <label class="game__answer  game__answer--photo">
-                    <input name="question2" type="radio" value="photo">
-                    <span>Фото</span>
-                  </label>
-                  <label class="game__answer  game__answer--paint">
-                    <input name="question2" type="radio" value="paint">
-                    <span>Рисунок</span>
-                  </label>
-                </div>
-              </form>
+        </form>
+        `;
+      break;
+    case `double`:
+      html = `
+          <p class="game__task">${level.question}</p>
+          <form class="game__content">
+            <div class="game__option">
+              <img src="${level.answers[0].image}" alt="Option 1" width="468" height="458">
+              <label class="game__answer game__answer--photo">
+                <input name="question1" type="radio" value="photo">
+                <span>Фото</span>
+              </label>
+              <label class="game__answer game__answer--paint">
+                <input name="question1" type="radio" value="paint">
+                <span>Рисунок</span>
+              </label>
             </div>
-            ${footerTemplate}
-          `);
-        break;
-      case `triple`:
-        html = getElementFromTemplate(`
-              <div class="game">
-                <p class="game__task">${level.question}</p>
-                <form class="game__content  game__content--triple">
-                  <div class="game__option">
-                    <img src="${level.answers[0].image}" alt="Option 1" width="304" height="455">
-                  </div>
-                  <div class="game__option  game__option--selected">
-                    <img src="${level.answers[1].image}" alt="Option 1" width="304" height="455">
-                  </div>
-                  <div class="game__option">
-                    <img src="${level.answers[2].image}" alt="Option 1" width="304" height="455">
-                  </div>
-                </form>
-              </div>
-              ${footerTemplate}
-            `);
-        break;
-    }
+            <div class="game__option">
+              <img src="${level.answers[1].image}" alt="Option 2" width="468" height="458">
+              <label class="game__answer  game__answer--photo">
+                <input name="question2" type="radio" value="photo">
+                <span>Фото</span>
+              </label>
+              <label class="game__answer  game__answer--paint">
+                <input name="question2" type="radio" value="paint">
+                <span>Рисунок</span>
+              </label>
+            </div>
+          </form>
+          `;
+      break;
+    case `triple`:
+      html = `
+          <p class="game__task">${level.question}</p>
+          <form class="game__content  game__content--triple">
+            <div class="game__option">
+              <img src="${level.answers[0].image}" alt="Option 1" width="304" height="455">
+            </div>
+            <div class="game__option  game__option--selected">
+              <img src="${level.answers[1].image}" alt="Option 1" width="304" height="455">
+            </div>
+            <div class="game__option">
+              <img src="${level.answers[2].image}" alt="Option 1" width="304" height="455">
+            </div>
+          </form>
+            `;
+      break;
   }
+
+  return html;
 }
 
-createGameLevel(generateLevelsData());
-renderHeader(html, gameState);
-renderStats(html.querySelector(`.game`));
+function renderGameLevel(index) {
+  const gameBox = gameContainer.querySelector(`.game`);
+  const level = gameData[index];
+  const gameLevel = fillGameLevel(level);
 
-export default html;
+  gameBox.innerHTML = ``;
+
+  gameBox.insertAdjacentHTML(`afterbegin`, gameLevel);
+}
+
+const gameData = generateLevelsData();
+const gameContainer = getElementFromTemplate(`<div class="game"></div>${footerTemplate}`);
+
+renderGameLevel(0);
+
+renderHeader(gameContainer, gameState);
+// renderStats(gameContainer.querySelector(`.game`));
+
+export default gameContainer;

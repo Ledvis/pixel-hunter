bbe82a88e6173d62ccfc5571bd5806560638799c
Zhenya <fedyai.eugen@gmail.com>
2018-07-06 18:14:45 +0300

    Add game header module

---
+++ js/game-header.js
@@ -0,0 +1,25 @@
+import {
+  getElementFromTemplate,
+} from './util';
+
+function createHeader(state) {
+  let html = getElementFromTemplate(`
+  <header class="header">
+    <div class="header__back"></div>
+    <h1 class="game__timer">${state.time}</h1>
+    <div class="game__lives">
+      ${new Array(3 - state.livesCount)
+        .fill(`<img src="img/heart__empty.svg" class="game__heart" alt="Life" width="32" height="32">`)
+        .join(``)}
+      ${new Array(state.livesCount)
+        .fill(`<img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">`)
+        .join(``)}
+    </div>
+  </header>
+`);
+  return html;
+}
+
+export function renderHeader(container, state) {
+  container.insertAdjacentElement(`beforebegin`, createHeader(state));
+}

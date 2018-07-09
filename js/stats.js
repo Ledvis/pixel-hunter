e048c3f6be0af0f1a801ed91223cdaede1c19b55
Zhenya <fedyai.eugen@gmail.com>
2018-07-09 17:12:47 +0300

    Refactor module stats

--- js/stats.js
+++ js/stats.js
@@ -1,16 +1,24 @@
-const html = `
-  <ul class="stats">
-    <li class="stats__result stats__result--wrong"></li>
-    <li class="stats__result stats__result--slow"></li>
-    <li class="stats__result stats__result--fast"></li>
-    <li class="stats__result stats__result--correct"></li>
-    <li class="stats__result stats__result--wrong"></li>
-    <li class="stats__result stats__result--unknown"></li>
-    <li class="stats__result stats__result--slow"></li>
-    <li class="stats__result stats__result--unknown"></li>
-    <li class="stats__result stats__result--fast"></li>
-    <li class="stats__result stats__result--unknown"></li>
-  </ul>
-`;
+import {
+  getElementFromTemplate
+} from './util';
 
-export default html;
+function createStats() {
+  return getElementFromTemplate(`
+    <ul class="stats">
+      <li class="stats__result stats__result--wrong"></li>
+      <li class="stats__result stats__result--slow"></li>
+      <li class="stats__result stats__result--fast"></li>
+      <li class="stats__result stats__result--correct"></li>
+      <li class="stats__result stats__result--wrong"></li>
+      <li class="stats__result stats__result--unknown"></li>
+      <li class="stats__result stats__result--slow"></li>
+      <li class="stats__result stats__result--unknown"></li>
+      <li class="stats__result stats__result--fast"></li>
+      <li class="stats__result stats__result--unknown"></li>
+    </ul>
+  `);
+}
+
+export default function renderStats(container) {
+  container.insertAdjacentElement(`beforeend`, createStats());
+}

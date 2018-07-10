22eed6c61effa00b8e20a438a3c7cbf9326055d8
Zhenya <fedyai.eugen@gmail.com>
2018-07-10 08:06:50 +0300

    Change static data to dynamic

--- js/results.js
+++ js/results.js
@@ -2,6 +2,7 @@ import {
   getElementFromTemplate
 } from './util';
 import footer from './footer-page';
+import countScore from './count-score';
 
 export default function renderResults(state) {
   return getElementFromTemplate(`
@@ -15,8 +16,8 @@ export default function renderResults(state) {
           <td class="result__number">1.</td>
           <td colspan="2">
             <ul class="stats">
-              <li class="stats__result stats__result--wrong"></li>
-              <li class="stats__result stats__result--slow"></li>
+              ${state.answers.map((answer) => `<li class="stats__result stats__result--${answer.isRight ? `correct` : `wrong`}">${answer.time}</li>`).join(``)}
+              <!-- <li class="stats__result stats__result--slow"></li>
               <li class="stats__result stats__result--fast"></li>
               <li class="stats__result stats__result--correct"></li>
               <li class="stats__result stats__result--wrong"></li>
@@ -24,11 +25,11 @@ export default function renderResults(state) {
               <li class="stats__result stats__result--slow"></li>
               <li class="stats__result stats__result--unknown"></li>
               <li class="stats__result stats__result--fast"></li>
-              <li class="stats__result stats__result--unknown"></li>
+              <li class="stats__result stats__result--unknown"></li> -->
             </ul>
           </td>
-          <td class="result__points">${state.answers.length * 100}</td>
-          <td class="result__total">900</td>
+          <td class="result__points">100</td>
+          <td class="result__total">${state.answers.length * 100}</td>
         </tr>
         <tr>
           <td></td>
@@ -52,59 +53,7 @@ export default function renderResults(state) {
           <td class="result__total">-100</td>
         </tr>
         <tr>
-          <td colspan="5" class="result__total  result__total--final">950</td>
-        </tr>
-      </table>
-      <table class="result__table">
-        <tr>
-          <td class="result__number">2.</td>
-          <td>
-            <ul class="stats">
-              <li class="stats__result stats__result--wrong"></li>
-              <li class="stats__result stats__result--slow"></li>
-              <li class="stats__result stats__result--fast"></li>
-              <li class="stats__result stats__result--correct"></li>
-              <li class="stats__result stats__result--wrong"></li>
-              <li class="stats__result stats__result--unknown"></li>
-              <li class="stats__result stats__result--slow"></li>
-              <li class="stats__result stats__result--wrong"></li>
-              <li class="stats__result stats__result--fast"></li>
-              <li class="stats__result stats__result--wrong"></li>
-            </ul>
-          </td>
-          <td class="result__total"></td>
-          <td class="result__total  result__total--final">fail</td>
-        </tr>
-      </table>
-      <table class="result__table">
-        <tr>
-          <td class="result__number">3.</td>
-          <td colspan="2">
-            <ul class="stats">
-              <li class="stats__result stats__result--wrong"></li>
-              <li class="stats__result stats__result--slow"></li>
-              <li class="stats__result stats__result--fast"></li>
-              <li class="stats__result stats__result--correct"></li>
-              <li class="stats__result stats__result--wrong"></li>
-              <li class="stats__result stats__result--unknown"></li>
-              <li class="stats__result stats__result--slow"></li>
-              <li class="stats__result stats__result--unknown"></li>
-              <li class="stats__result stats__result--fast"></li>
-              <li class="stats__result stats__result--unknown"></li>
-            </ul>
-          </td>
-          <td class="result__points">×&nbsp;100</td>
-          <td class="result__total">900</td>
-        </tr>
-        <tr>
-          <td></td>
-          <td class="result__extra">Бонус за жизни:</td>
-          <td class="result__extra">2&nbsp;<span class="stats__result stats__result--alive"></span></td>
-          <td class="result__points">×&nbsp;50</td>
-          <td class="result__total">100</td>
-        </tr>
-        <tr>
-          <td colspan="5" class="result__total  result__total--final">950</td>
+          <td colspan="5" class="result__total  result__total--final">${countScore(state.answers, state.livesCount)}</td>
         </tr>
       </table>
     </div>

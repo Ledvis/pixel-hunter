54a6ef0d0af5fdbfd0160dfd525585b505f53098
Zhenya <fedyai.eugen@gmail.com>
2018-07-13 13:20:31 +0300

    Change stats dynamicaly

--- js/stats.js
+++ js/stats.js
@@ -2,25 +2,19 @@ import {
   getElementFromTemplate
 } from './util';
 
-function createStats() {
+function createStats(state) {
   return getElementFromTemplate(`
     <div class="stats">
       <ul class="stats">
-        <li class="stats__result stats__result--wrong"></li>
+        ${state.answers.map((answer) => `<li class="stats__result stats__result--${answer.isRight ? `correct` : `wrong`}"></li>`).join(``)}
         <li class="stats__result stats__result--slow"></li>
         <li class="stats__result stats__result--fast"></li>
-        <li class="stats__result stats__result--correct"></li>
-        <li class="stats__result stats__result--unknown"></li>
-        <li class="stats__result stats__result--unknown"></li>
-        <li class="stats__result stats__result--unknown"></li>
-        <li class="stats__result stats__result--unknown"></li>
-        <li class="stats__result stats__result--unknown"></li>
         <li class="stats__result stats__result--unknown"></li>
       </ul>
     </div>
   `);
 }
 
-export default function renderStats(container) {
-  container.insertAdjacentElement(`beforeend`, createStats());
+export default function renderStats(container, state) {
+  container.insertAdjacentElement(`beforeend`, createStats(state));
 }

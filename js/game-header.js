53fc95629b41c8d345c537eafdd71ae42d15a472
Zhenya <fedyai.eugen@gmail.com>
2018-07-19 15:25:59 +0300

    Try to refactor game header module

--- js/game-header.js
+++ js/game-header.js
@@ -1,25 +1,30 @@
 import {
   getElementFromTemplate,
 } from './util';
+import renderBtnBack from './btn-back';
 
 function createHeader(state) {
-  let html = getElementFromTemplate(`
-  <header class="header">
-    <div class="header__back"></div>
-    <h1 class="game__timer">${state.time}</h1>
-    <div class="game__lives">
-      ${new Array(3 - state.livesCount)
-        .fill(`<img src="img/heart__empty.svg" class="game__heart" alt="Life" width="32" height="32">`)
-        .join(``)}
-      ${new Array(state.livesCount)
-        .fill(`<img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">`)
-        .join(``)}
-    </div>
-  </header>
-`);
+  let html = getElementFromTemplate(`<header class="header">
+      <div class="header__back">
+      </div>
+      <h1 class="game__timer">${state.time}</h1>
+      <div class="game__lives">
+        ${new Array(3 - state.livesCount)
+          .fill(`<img src="img/heart__empty.svg" class="game__heart" alt="Life" width="32" height="32">`)
+          .join(``)}
+        ${new Array(state.livesCount)
+          .fill(`<img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">`)
+          .join(``)}
+      </div>
+    </header>
+  `);
   return html;
 }
 
 export function renderHeader(container, state) {
-  container.insertAdjacentElement(`beforebegin`, createHeader(state));
+  if (!document.querySelector(`.header`)) {
+    const headerContainer = createHeader(state);
+    renderBtnBack(headerContainer);
+    container.insertAdjacentElement(`beforebegin`, headerContainer);
+  }
 }
